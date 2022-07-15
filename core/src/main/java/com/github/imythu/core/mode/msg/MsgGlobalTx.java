fbda55a2e1c4beb5dcf468ecbf005defe3a4ce6a
imythu <imythu@foxmail.com>
2022-07-15 17:24:07 +0800

    save

---
+++ core/src/main/java/com/github/imythu/core/mode/msg/MsgGlobalTx.java
@@ -0,0 +1,15 @@
+package com.github.imythu.core.mode.msg;
+
+
+import com.github.imythu.core.exception.PrepareException;
+
+public interface MsgGlobalTx {
+    /**
+     * 开启全局事务
+     *
+     * @throws PrepareException 开启失败抛出异常
+     */
+    void prepare() throws PrepareException;
+
+
+}
